34b1654964e66eaf5bdc81a8258c56d5260cb48a
Robert Shaw <r.shaw1@yahoo.com>
2018-01-29 22:45:28 -0600

    made search component stateful

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,45 @@
 'use strict';
 
 /** NODE MODULES */
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Input } from 'semantic-ui-react';
 
 /** LOCAL MODULES */
 
+
 // Search Component
-export default function Search() {
-  return (
-    <div>
-      <h1>You've reached the Search Component!</h1>
-      <br/> 
-      <Link to={`/businessdetail/${'insert-business-name'}`}>
-        <h3>Click Here if you'd like to see the Business Detail Component</h3>
-      </Link>
-    </div>
-  )
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    }
+  }
+
+  handleChange = (e) => {
+    const searchTerm = e.target.value;
+    this.setState({searchTerm})
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>You've reached the Search Component!</h1>
+        <br/>
+          <Input
+            size='large'
+            icon='search'
+            placeholder='Search...'
+            fluid
+            onChange={this.handleChange}
+          />
+        <br/>
+        <Link to={`/businessdetail/${'insert-business-name'}`}>
+          <h3>Click Here if you'd like to see the Business Detail Component</h3>
+        </Link>
+      </div>
+    )
+  }
 }
