c00706e527314d401a5c99927c8e39747ac67017
Robert Shaw <r.shaw1@yahoo.com>
2018-01-30 16:00:25 -0600

    setup express server

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+'use strict'
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('../../webpack.config.js')(process.env);
+const compiler = webpack(config);
+
+/*
+ Tell express to use the webpack-dev-middleware and use the webpack.config.js file as a base.
+ */
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+const PUBLIC_DIR = path.resolve(__dirname, '../../public');
+const PORT = 8080;
+
+app.use(express.static(PUBLIC_DIR));
+app.use('*', express.static(PUBLIC_DIR));
+
+
+app.listen(PORT, (err) => {
+  if(err) throw err;
+  console.log(`Server running on PORT ${PORT}!\n`);
+});
