587c57793f858fcc77f16a2b0b0321c538d91a57
Robert Shaw <r.shaw1@yahoo.com>
2018-01-31 00:52:12 -0600

    added a fixed footer with attribution

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -9,28 +9,51 @@ import {
 } from 'react-router-dom';
 
 /** LOCAL MODULES */
+import Yelp from '../../../public/assets/Yelp_trademark_RGB_outline.png'
 import Search from './Search';
 import BusinessDetail from './BusinessDetail';
 
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '3rem',
+const styles = {
+  containerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '3rem',
+  },
+  scrollStyle: {
+    paddingBottom: '6.5rem',
+  },
+  footerStyle: {
+    marginTop: 'auto',
+    paddingBottom: '.5rem',
+    width: '100%',
+    backgroundColor: 'red',
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'fixed',
+    left: '0',
+    bottom: '0',
+  },
 }
 
 //Create App component
 export default function App() {
   return (
     <Router>
-      <div className='container' style={containerStyle}>
-        <Switch>
-          <Route exact path='/' component={Search} />
-          <Route path='/business-detail/:id' component={BusinessDetail} />
-          <Route render={function() {
-              return <p>Not Found</p>
-          }} />
-        </Switch>
+        <div className='container' style={styles.containerStyle}>
+          <div style={styles.scrollStyle}>
+            <Switch>
+              <Route exact path='/' component={Search} />
+              <Route path='/business-detail/:id' component={BusinessDetail} />
+              <Route render={function() {
+                  return <p>Not Found</p>
+              }} />
+            </Switch>
+          </div>
+          <footer style={styles.footerStyle}>
+            <span className='powered'>Powered by</span>
+            <img className='yelp-logo' src={Yelp} alt='Yelp' />
+          </footer>
       </div>
     </Router>
   )
