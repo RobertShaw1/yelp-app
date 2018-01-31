a84b91504339ff710bd492bbef7a57d512dbc325
Robert Shaw <r.shaw1@yahoo.com>
2018-01-30 20:33:40 -0600

    created api endpoints

--- src/server/api.js
+++ src/server/api.js
@@ -1,11 +1,21 @@
 'use strict'
 
+const { fetchBusinesses, fetchBusinessDetail } = require('./utils');
 const api = require('express').Router();
 
 module.exports = api;
 
-api.get('/search', (req, res) => {
-  res.send(`Made it! and searched ${req.query.term}`)
+
+api.get('/search', (req, res, next) => {
+  fetchBusinesses(req.query.term)
+  .then(data => res.json(data))
+  .catch(next)
+});
+
+api.get('/business-detail/:id', (req, res, next) => {
+  fetchBusinessDetail(req.params.id)
+  .then(data => res.json(data))
+  .catch(next)
 });
 
 api.use((req, res, next) => {
@@ -13,4 +23,3 @@ api.use((req, res, next) => {
   error.status = 404;
   next(error);
 });
-
