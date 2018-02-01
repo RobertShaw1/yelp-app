3cb70c0b764d82ef8889d263f24e0da43fb1b951
Robert Shaw <r.shaw1@yahoo.com>
2018-01-31 22:01:42 -0600

    refactored containers and added link to Yelp

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -9,39 +9,16 @@ import {
 } from 'react-router-dom';
 
 /** LOCAL MODULES */
-import Yelp from '../../../public/assets/Yelp_trademark_RGB_outline.png'
 import Search from './Search';
 import BusinessDetail from './BusinessDetail';
-
-const styles = {
-  containerStyle: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    paddingTop: '3rem',
-  },
-  scrollStyle: {
-    paddingBottom: '6.5rem',
-  },
-  footerStyle: {
-    marginTop: 'auto',
-    paddingBottom: '.5rem',
-    width: '100%',
-    backgroundColor: 'red',
-    display: 'flex',
-    justifyContent: 'center',
-    position: 'fixed',
-    left: '0',
-    bottom: '0',
-  },
-}
+import Yelp from '../../../public/assets/Yelp_trademark_RGB_outline.png'
 
 //Create App component
 export default function App() {
   return (
     <Router>
-        <div className='container' style={styles.containerStyle}>
-          <div style={styles.scrollStyle}>
+        <div className='main-container'>
+          <div className='scroll-container'>
             <Switch>
               <Route exact path='/' component={Search} />
               <Route path='/business-detail/:id' component={BusinessDetail} />
@@ -50,9 +27,11 @@ export default function App() {
               }} />
             </Switch>
           </div>
-          <footer style={styles.footerStyle}>
+          <footer>
             <span className='powered'>Powered by</span>
-            <img className='yelp-logo' src={Yelp} alt='Yelp' />
+            <a href="http://www.yelp.com" target='_blank'>
+              <img className='yelp-logo' src={Yelp} alt='Yelp' />
+            </a>
           </footer>
       </div>
     </Router>
