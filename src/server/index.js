7d2193f3f5704228a8805b582e13f54057468957
Robert Shaw <r.shaw1@yahoo.com>
2018-02-01 13:43:34 -0600

    updates for deployment
    updated PORT for server
    
    server update
    
    webpack
    
    reconfigured server for production
    
    added favicon
    
    html fix
    
    squashed commits

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,34 @@
 'use strict'
 
-require('../../secrets');
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
 const app = express();
-const config = require('../../webpack.config.js')(process.env);
-const compiler = webpack(config);
 
-/*
-Tell express to use the webpack-dev-middleware and use the webpack.config.js file as a base.
-*/
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}));
+if (process.env.NODE_ENV !== 'production') {
+  require('../../secrets');
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const config = require('../../webpack.config.js')(process.env);
+  const compiler = webpack(config);
+  /*
+  Tell express to use the webpack-dev-middleware and use the webpack.config.js file as a base.
+  */
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PUBLIC_DIR = path.resolve(__dirname, '../../public');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use('/api', require('./api'));
 app.use(express.static(PUBLIC_DIR));
-
 
 app.listen(PORT, (err) => {
   if(err) throw err;
